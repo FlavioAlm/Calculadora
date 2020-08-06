9ff0f048d52ccdecf0ca718888283c15d7e1bc3f
flavioalm <flavioemx@gmail.com>
2020-08-06 14:37:22 -0300

    versao linear: buttons e visor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+var conta = "";
+
+const Button = (props) => {
+
+  function handleClick(event){
+    alert("Clicou em: " + event.target.innerText);
+    conta += (event.target.innerText);
+
+    console.log(conta);
+
+    document.getElementById("result").innerHTML = conta.split();
+  };
+
+  return (
+    <button onClick={handleClick}>{props.name}{conta}</button>
+  );
+}
+
+const Result = () => {
+
+  return ( 
+    <textarea id="result"></textarea>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>Calculadora em construção ...</h1>
+        <Button name="0" /><br/>
+        <Button name="1" /><br/>
+        <Button name="+" /><br/>
+        <Button name="=" /><br/>
+        <Result />
     </div>
   );
 }
